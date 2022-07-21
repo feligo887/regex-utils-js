ad85104d5886ba7f3269db3f4f311ac77e90ab22
only丶炀 <1763645020@qq.com>
2022-07-22 00:11:28 +0800

    feat: project init

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+
+export default defineConfig((options) => {
+    return {
+        entry: ['src/index.ts'],
+        splitting: false,
+        outDir: 'dist',
+        format: ['esm', 'cjs'],
+        sourcemap: !!options.watch,
+        clean: true,
+        minify: !options.watch,
+    }
+})
