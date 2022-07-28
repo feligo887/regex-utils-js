1c70bc2b8d2f7187c238252214307cee956ff302
only丶炀 <1763645020@qq.com>
2022-07-28 21:20:21 +0800

    test: ✅   通过联系电话相关的单测

--- tests/tel.test.ts
+++ tests/tel.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { loosePhoneReg, strictPhoneReg } from '../src';
+import {  loosePhoneReg, strictPhoneReg, telPhoneReg } from '../src';
 
 describe ('宽松手机号码校验测试', () => {
     it ('普通手机号码测试:', () => {
@@ -50,4 +50,35 @@ describe ('严格手机号码校验测试', () => {
         expect( strictPhoneReg ( '+08613898786754', true ) ).toBeTruthy ();
         expect( strictPhoneReg ( '+8615898786754', true ) ).toBeTruthy ();
     })
+});
+
+describe ('国内固定电话号码测试', () => {
+    it ('固定电话号码格式测试:', () => {
+        expect( telPhoneReg ( '04098786544' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '040-276-87789877' ) ).toBeFalsy ();
+        expect( telPhoneReg ( 'abc-028-1234' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '400-7898' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '400-789897799' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '4000-jhjh' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '4000-jhjh1237' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '04000-78989779' ) ).toBeFalsy ();
+        expect( telPhoneReg ( '0400-78989779' ) ).toBeTruthy ();
+        expect( telPhoneReg ( '0280-78989779' ) ).toBeTruthy ();
+        expect( telPhoneReg ( '028-98678766' ) ).toBeTruthy ();
+    })
+
+    it ('固定电话号码-分机号码格式测试:', () => {
+        expect( telPhoneReg ( '040-276-87789877', true ) ).toBeFalsy ();
+        expect( telPhoneReg ( 'abc-028-1234', true  ) ).toBeFalsy ();
+        expect( telPhoneReg ( '400-7898' , true ) ).toBeFalsy ();
+        expect( telPhoneReg ( '400-789897799', true  ) ).toBeFalsy ();
+        expect( telPhoneReg ( '4000-jhjh' , true)  ).toBeFalsy ();
+        expect( telPhoneReg ( '4000-jhjh1237', true  ) ).toBeFalsy ();
+        expect( telPhoneReg ( '04000-78989779', true ) ).toBeFalsy ();
+        expect( telPhoneReg ( '028-98678766-8988900', true ) ).toBeFalsy ();
+        expect( telPhoneReg ( '028-78989779-123', true ) ).toBeTruthy ();
+        expect( telPhoneReg ( '0280-78989779-5687', true ) ).toBeTruthy ();
+        expect( telPhoneReg ( '028-9867876-898800', true ) ).toBeTruthy ();
+    })
+
 });
