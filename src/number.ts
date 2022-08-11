209421366408cc882a8b0a1c21d0046da48fa66d
only丶炀 <1763645020@qq.com>
2022-08-11 23:34:30 +0800

    feat: 增加小数校验正则

--- src/number.ts
+++ src/number.ts
@@ -1,7 +1,6 @@
-
 /**
  * 检查数字精度，整数大于0，小数点最高可达8位19.8n精度
- *@param { string } str 需要检查的数字
+ *@param { string } str 需要检查的字符串
  * **/
 
 export function numberOrFloatReg ( str: string ): boolean {
@@ -9,5 +8,23 @@ export function numberOrFloatReg ( str: string ): boolean {
     const reg = /^[1-9]+([.]{1}[0-9]{1,8})?$/;
 
    return reg.test ( str );
+
+}
+
+/**
+ * 严格小数正则校验, 支持负数
+ * @param { string } str 需要检查的字符串
+ * @param { Object } options 配置项
+ * @param { number } options.decimals 支持的小数位数，默认两位小数
+ * @param { boolean } options.minus 是否支持负数，默认false
+ * **/
+
+export function strictDecimalsReg ( str: string, options?: { decimals?: number, minus?: false } ): boolean {
+
+    const reg = `^(-)?(0+(.[1-9]){1})|([1-9]+(.[0-9]{1,2})$)`;
+
+    console.log( reg );
+
+    return RegExp ( reg, 'g' ).test ( str );
 
 }
--- tests/number.test.ts
+++ tests/number.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-import { numberOrFloatReg } from '../src';
+import { numberOrFloatReg, strictDecimalsReg } from '../src';
 
 describe ('金额正则测试', () => {
     it ('19.8n正数金额测试', () => {
@@ -10,5 +10,16 @@ describe ('金额正则测试', () => {
         expect( numberOrFloatReg ('111.888888888' ) ).toBeFalsy ();
         expect( numberOrFloatReg ('111.88888888' ) ).toBeTruthy ();
         expect( numberOrFloatReg ('19.8' ) ).toBeTruthy ();
+    })
+})
+
+describe ('小数严格正则测试', () => {
+    it ('合法性测试', () => {
+        expect( strictDecimalsReg ('0.' ) ).toBeFalsy ();
+        expect( strictDecimalsReg ('12.' ) ).toBeFalsy ();
+        expect( strictDecimalsReg ('0.01' ) ).toBeTruthy ();
+        expect( strictDecimalsReg ('00.0123' ) ).toBeTruthy ();
+        expect( strictDecimalsReg ('00.0123' ) ).toBeTruthy ();
+        expect( strictDecimalsReg ('11.22' ) ).toBeTruthy ();
     })
 })
