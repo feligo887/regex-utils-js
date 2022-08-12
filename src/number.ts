b6297344f797f797ad7d244732095c20706bcbb7
only丶炀 <1763645020@qq.com>
2022-08-12 22:18:29 +0800

    更新正则

--- src/number.ts
+++ src/number.ts
@@ -1,3 +1,5 @@
+import {loosePhoneReg} from "./common";
+
 /**
  * 检查数字精度，整数大于0，小数点最高可达8位19.8n精度
  *@param { string } str 需要检查的字符串
@@ -12,18 +14,49 @@ export function numberOrFloatReg ( str: string ): boolean {
 }
 
 /**
+ * 整数正则校验
+ * @description
+ * @param { string } str 需要检查的字符串
+ * @param { boolean } minus 是否支持负数
+ * @return { boolean } 是否匹配
+ * **/
+
+export function  integerReg ( str: string, minus?: boolean ): boolean {
+
+    const reg = `^${ minus ? '(?:-)' : '' }[1-9]+[0-9]*$`;
+
+    return RegExp( reg ).test ( str );
+
+}
+
+/**
+ * 弱小数正则校验, 支持负数
+ * @description 只要是 xxx.xxx、-xxx.xxx 格式均可通过
+ * @param { string } str 需要检查的字符串
+ * @param { number } decimalsMax 最大小数位. 默认为8
+ * @return { boolean } 是否匹配
+ * **/
+
+export function looseDecimalsReg ( str: string, decimalsMax?: number ): boolean {
+
+    const reg = `^(-)?\\d+\.{0,1}\\d{1,${ decimalsMax || 8 }}$`;
+
+    return RegExp ( reg, 'g' ).test ( str );
+
+}
+
+/**
  * 严格小数正则校验, 支持负数
+ * @description 01.xxx 00.xxx 不可通过
  * @param { string } str 需要检查的字符串
  * @param { Object } options 配置项
  * @param { number } options.decimals 支持的小数位数，默认两位小数
  * @param { boolean } options.minus 是否支持负数，默认false
  * **/
 
-export function strictDecimalsReg ( str: string, options?: { decimals?: number, minus?: false } ): boolean {
+export function strictDecimalsReg ( str: string, options?: { decimals?: number, minus?: boolean } ): boolean {
 
-    const reg = `^(-)?(0+(.[1-9]){1})|([1-9]+(.[0-9]{1,2})$)`;
-
-    console.log( reg );
+    const reg = `^${ ( options && options.minus === true ) ? '(-)?' : '' }(\\0{0,1}|[1-9])+\\.{0,1}\\d{1,${ options?.decimals || 2 }}$`;
 
     return RegExp ( reg, 'g' ).test ( str );
 
--- tests/number.test.ts
+++ tests/number.test.ts
@@ -1,25 +1,57 @@
 import { describe, expect, it } from "vitest";
 
-import { numberOrFloatReg, strictDecimalsReg } from '../src';
+import { numberOrFloatReg, integerReg, strictDecimalsReg, looseDecimalsReg } from '../src';
 
 describe ('金额正则测试', () => {
     it ('19.8n正数金额测试', () => {
         expect( numberOrFloatReg ('0.' ) ).toBeFalsy ();
         expect( numberOrFloatReg ('12.' ) ).toBeFalsy ();
         expect( numberOrFloatReg ('0.8' ) ).toBeFalsy ();
+        expect( numberOrFloatReg ('0..8' ) ).toBeFalsy ();
         expect( numberOrFloatReg ('111.888888888' ) ).toBeFalsy ();
         expect( numberOrFloatReg ('111.88888888' ) ).toBeTruthy ();
         expect( numberOrFloatReg ('19.8' ) ).toBeTruthy ();
     })
 })
 
-describe ('小数严格正则测试', () => {
+describe ('整数正则测试', () => {
+
+    it ('合法整数测试', () => {
+        expect(  integerReg ('0.' ) ).toBeFalsy ();
+        expect(  integerReg ('12.88' ) ).toBeFalsy ();
+        expect(  integerReg ('-12.88' ) ).toBeFalsy ();
+        expect(  integerReg ('012' ) ).toBeFalsy ();
+        expect(  integerReg ('0012' ) ).toBeFalsy ();
+        expect(  integerReg ('-12' ) ).toBeFalsy ();
+        expect(  integerReg ('12' ) ).toBeTruthy ();
+        expect(  integerReg ('102' ) ).toBeTruthy ();
+        expect(  integerReg ('-102', true ) ).toBeTruthy ();
+    })
+})
+
+describe ('小数正则测试', () => {
+
+    it ('弱校验小数测试', () => {
+        expect( looseDecimalsReg ('0.' ) ).toBeFalsy ();
+        expect(  looseDecimalsReg ('12.' ) ).toBeFalsy ();
+        expect( looseDecimalsReg ('12.21' ) ).toBeTruthy ();
+        expect( looseDecimalsReg ('00.0123' ) ).toBeTruthy ();
+        expect( looseDecimalsReg ('00.0123' ) ).toBeTruthy ();
+        expect( looseDecimalsReg ('11.22' ) ).toBeTruthy ();
+        expect( looseDecimalsReg ('-11.22' ) ).toBeTruthy ();
+        expect( looseDecimalsReg ('-0.22' ) ).toBeTruthy ();
+    })
+
     it ('合法性测试', () => {
         expect( strictDecimalsReg ('0.' ) ).toBeFalsy ();
         expect( strictDecimalsReg ('12.' ) ).toBeFalsy ();
-        expect( strictDecimalsReg ('0.01' ) ).toBeTruthy ();
-        expect( strictDecimalsReg ('00.0123' ) ).toBeTruthy ();
-        expect( strictDecimalsReg ('00.0123' ) ).toBeTruthy ();
-        expect( strictDecimalsReg ('11.22' ) ).toBeTruthy ();
+        expect( strictDecimalsReg ('0.0123' ) ).toBeFalsy ();
+        expect( strictDecimalsReg ('00.0123' ) ).toBeFalsy ();
+        expect( strictDecimalsReg ('013.88' ) ).toBeFalsy ();
+        expect( strictDecimalsReg ('-0.0123' ) ).toBeFalsy ();
+
+        // 需要处理此问题 TODO
+        // expect( strictDecimalsReg ('-0.01', { minus: true } ) ).toBeTruthy ();
+        expect( strictDecimalsReg ('12.21' ) ).toBeTruthy ();
     })
 })
