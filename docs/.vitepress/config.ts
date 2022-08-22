fcefe10d7dcd9ab63995e0fb4ddeebf4583d6431
only丶炀 <1763645020@qq.com>
2022-08-22 21:26:42 +0800

    docs: 📝   文档结构更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,8 +20,49 @@ export default defineConfig ( {
   themeConfig: {
     nav: [
       { text: '引导', link: '/guide/foreword', activeMatch: '/guide/' },
-      { text: 'API', link: '/apis/', activeMatch: '/apis/' },
       { text: '更新日志', link: 'https://github.com/zguiyang/regex-utils-js' },
     ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: '引导',
+          collapsed: true,
+          collapsible: false,
+          items: [
+            {
+              text: '前言',
+              link: '/guide/foreword',
+            },
+            {
+              text: '使用指南',
+              link: '/guide/getting-started',
+            },
+          ],
+        },
+        {
+          text: 'API',
+          collapsed: true,
+          collapsible: false,
+          items: [
+            {
+              text: '常用正则',
+              link: '/guide/apis/common',
+            },
+            {
+              text: '数字正则',
+              link: '/guide/apis/number',
+            },
+            {
+              text: '字符串正则',
+              link: '/guide/apis/string',
+            },
+          ],
+        },
+      ],
+    },
+    editLink: {
+      pattern: 'https://github.com/zguiyang/regex-utils-js/tree/main/docs/:path',
+      text: '编辑此页',
+    },
   },
 } );
